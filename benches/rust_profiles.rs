a9862589bb2164e1cd7189ca33b5c1c95eb57e0a
Cyberphinx <88593809+Cyberphinx@users.noreply.github.com>
2025-10-30 05:09:43 +0000

    feat: added struct of arrays SOA

--- benches/rust_profiles.rs
+++ benches/rust_profiles.rs
@@ -1,6 +1,5 @@
-use std::hint::black_box;
-
 use criterion::{Criterion, criterion_group, criterion_main};
+use std::hint::black_box;
 
 #[derive(Clone)]
 #[repr(Rust)]
@@ -22,6 +21,32 @@ pub struct CRepr {
     pub tiny: bool,
 }
 
+#[derive(Clone)]
+pub struct StructOfVecs {
+    pub longs: Vec<u64>,
+    pub normals: Vec<u32>,
+    pub shorts: Vec<u16>,
+    pub smalls: Vec<u8>,
+    pub tinies: Vec<bool>,
+}
+
+impl StructOfVecs {
+    pub fn new(n: usize) -> Self {
+        Self {
+            longs: vec![999u64; n],
+            normals: vec![123u32; n],
+            shorts: vec![7u16; n],
+            smalls: vec![42u8; n],
+            tinies: vec![true; n],
+        }
+    }
+
+    #[inline(always)]
+    pub fn len(&self) -> usize {
+        self.longs.len()
+    }
+}
+
 fn rust_layout(c: &mut Criterion) {
     let data = vec![
         RustRepr {
@@ -29,7 +54,7 @@ fn rust_layout(c: &mut Criterion) {
             normal: 123,
             small: 42,
             long: 999,
-            short: 7
+            short: 7,
         };
         1_000_000
     ];
@@ -51,7 +76,7 @@ fn c_layout(c: &mut Criterion) {
             normal: 123,
             small: 42,
             long: 999,
-            short: 7
+            short: 7,
         };
         1_000_000
     ];
@@ -66,5 +91,20 @@ fn c_layout(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, rust_layout, c_layout);
+fn struct_of_vecs_layout(c: &mut Criterion) {
+    const N: usize = 1_000_000;
+    let data = StructOfVecs::new(N);
+
+    c.bench_function("Struct of Vecs", |b| {
+        b.iter(|| {
+            let mut s = 0u64;
+            for i in 0..black_box(data.len()) {
+                s += data.longs[i] ^ data.normals[i] as u64;
+            }
+            black_box(s);
+        })
+    });
+}
+
+criterion_group!(benches, rust_layout, c_layout, struct_of_vecs_layout);
 criterion_main!(benches);
